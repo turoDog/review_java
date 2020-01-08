531d80707bd6b949037d91c139a52a522f2414d7
turoDog <turodog@foxmail.com>
2020-01-08 14:37:18 +0800

    :sparkles: lambda 表达式为什么不可变？

--- src/main/java/com/nasus/lambda/how/VariableScopeTest.java
+++ src/main/java/com/nasus/lambda/how/VariableScopeTest.java
@@ -23,6 +23,22 @@ public class VariableScopeTest {
         // 输出结果为 3
         s.convert(2);
 
+        // lambda 表达式的局部变量可以不用声明为 final，但是必须不可被后面的代码修改（即隐性的具有 final 的语义）
+        // 否则报错
+        /*final int num = 1;
+        Converter<Integer, String> s = (param) -> System.out.println(String.valueOf(param + num));
+        // 输出结果为 3
+        s.convert(2);
+        num = 10;*/
+
+        // 为什么不能改变？
+        /*int num = 1;
+        // 这一步只是声明了 lambda ，并没有执行
+        Converter<Integer, String> s = (param) -> System.out.println(String.valueOf(param + num));
+        // 假设中途局部变量改变
+        num ++;
+        // 这时候再执行 lambda ，结果就变了，这就造成数据不同步问题。
+        s.convert(2);*/
     }
 
 }
